7533fadfe3bdc567e2827f9887a1be5f9f8aa63a
Maxim Oskolsky <oskolsky.maxim@gmail.com>
2025-06-27 14:36:42 +0300

    Fix content page layout

--- src/components/features/content/content.tsx
+++ src/components/features/content/content.tsx
@@ -15,35 +15,30 @@ type ContentProps = IContent
 
 export const Content = (props: ContentProps) => {
     return (
-        <div className="container py-12">
-            <div className="space-y-8 rounded bg-white px-4 py-8 shadow-xs sm:w-3/4">
-                <div className="space-y-4">
-                    <div className="flex flex-col gap-y-2">
-                        <ContentTitle name={props.name} />
-                        <ContentAuthor
-                            month={props.month}
-                            year={props.year}
-                            type={props.type}
-                            comedians={props.comedians}
-                            group={props.group}
-                        />
-                    </div>
+        <div className="wrapper-sm py-8 sm:py-16">
+            <div className="flex flex-col gap-y-4 sm:gap-y-8">
+                <div className="flex flex-col gap-y-2">
+                    <ContentTitle name={props.name} />
+                    <ContentAuthor
+                        month={props.month}
+                        year={props.year}
+                        type={props.type}
+                        comedians={props.comedians}
+                        group={props.group}
+                    />
+                </div>
 
-                    <div className="flex items-center justify-between rounded bg-gray-100 p-4">
-                        <div className="flex items-center gap-x-6">
-                            <ContentRating
-                                avgRating={props.rating.avgRating}
-                                reviewsCount={props.rating.reviewsCount}
-                            />
-                            <ContentMyRating />
-                        </div>
-                        <div className="flex items-center gap-x-6">
-                            <ContentAddToWatchList contentId={props.id} isInWatchlist={!!props.watchlists?.length} />
-                            <ContentPlay duration={props.duration} />
-                        </div>
-                    </div>
+                <ContentCover cover={props.contentImages[0]?.url} name={props.name} />
 
-                    <ContentCover cover={props.contentImages[0]?.url} name={props.name} />
+                <div className="flex items-center justify-between rounded bg-gray-50 p-4">
+                    <div className="flex items-center gap-x-6">
+                        <ContentRating avgRating={props.rating.avgRating} reviewsCount={props.rating.reviewsCount} />
+                        <ContentMyRating />
+                    </div>
+                    <div className="flex items-center gap-x-6">
+                        <ContentAddToWatchList contentId={props.id} isInWatchlist={!!props.watchlists?.length} />
+                        <ContentPlay duration={props.duration} />
+                    </div>
                 </div>
 
                 <ContentDescription description={props.metaInfo?.description} />
