18b10abb33aa018361887982f775a0b0a76f7afd
Maxim Oskolsky <oskolsky.maxim@gmail.com>
2025-06-30 12:32:52 +0300

    fix: about ratings

--- src/components/features/layout/layout/layout.tsx
+++ src/components/features/layout/layout/layout.tsx
@@ -1,7 +1,4 @@
 import classNames from 'classnames'
-import { ChevronsRightIcon } from 'lucide-react'
-
-import Link from 'next/link'
 
 import { LayoutNav } from './components/layout-nav'
 
@@ -22,7 +19,7 @@ export const Layout = ({ children, title, size, nav }: LayoutProps) => {
     return (
         <div className={classNames(wrapperSize, 'py-8 sm:py-16')}>
             <div className="flex flex-col gap-y-8 sm:gap-y-8">
-                <nav className="flex items-center gap-x-1">
+                {/* <nav className="flex items-center gap-x-1">
                     <Link href="/" className="text-sm text-gray-500! no-underline! hover:text-black!">
                         Главная
                     </Link>
@@ -38,7 +35,7 @@ export const Layout = ({ children, title, size, nav }: LayoutProps) => {
                     <Link href="/contents" className="text-sm text-gray-500! no-underline! hover:text-black!">
                         Рецензии
                     </Link>
-                </nav>
+                </nav> */}
 
                 <h1 className="mb-0!">{title}</h1>
                 <LayoutNav items={nav} />
