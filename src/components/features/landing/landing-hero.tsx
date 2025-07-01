6fe148e50560a36247f018ca134b383fd77a7952
Maxim Oskolsky <oskolsky.maxim@gmail.com>
2025-07-01 10:00:56 +0300

    Hero block

--- src/components/features/landing/landing-hero.tsx
+++ src/components/features/landing/landing-hero.tsx
@@ -1,24 +1,17 @@
 'use client'
 
+import Image from 'next/image'
+
 export const LandingHero = () => {
     return (
-        <div>Carousel</div>
-        // <Carousel className="w-full rounded-lg bg-gray-500">
-        //     <Carousel.Item>
-        //         <div className="h-[400px] w-full">
-        //             <Image src="/images/hero/hero-1.webp" alt="Landing Hero" fill className="rounded-lg object-cover" />
-        //         </div>
-        //     </Carousel.Item>
-        //     <Carousel.Item>
-        //         <div className="h-[400px] w-full">
-        //             <Image src="/images/hero/hero-2.webp" alt="Landing Hero" fill className="rounded-lg object-cover" />
-        //         </div>
-        //     </Carousel.Item>
-        //     <Carousel.Item>
-        //         <div className="h-[400px] w-full">
-        //             <Image src="/images/hero/hero-3.jpg" alt="Landing Hero" fill className="rounded-lg object-cover" />
-        //         </div>
-        //     </Carousel.Item>
-        // </Carousel>
+        <div className="flex items-center justify-center bg-black sm:pt-10 sm:pb-20">
+            <Image
+                src="/images/hero/hero-2.webp"
+                alt="Landing Hero"
+                width={1216}
+                height={400}
+                className="align-top sm:rounded-xl"
+            />
+        </div>
     )
 }
--- src/components/features/landing/landing.tsx
+++ src/components/features/landing/landing.tsx
@@ -1,12 +1,16 @@
+import { LandingHero } from './landing-hero'
 import { LandingNew } from './landing-new'
 import { LandingRecommended } from './landing-recommended'
 
 export const Landing = () => {
     return (
-        <div className="wrapper-lg py-8 sm:py-16">
-            <div className="flex flex-col gap-y-6 sm:gap-y-12">
-                <LandingNew />
-                <LandingRecommended />
+        <div>
+            <LandingHero />
+            <div className="wrapper-lg py-8 sm:py-16">
+                <div className="flex flex-col gap-y-6 sm:gap-y-12">
+                    <LandingNew />
+                    <LandingRecommended />
+                </div>
             </div>
         </div>
     )
