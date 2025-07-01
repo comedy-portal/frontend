e3495ef518736ba38669cd88b1c07886c7a40f0c
Maxim Oskolsky <oskolsky.maxim@gmail.com>
2025-07-01 09:35:39 +0300

    Global loadedr

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,3 +1,10 @@
+import { MicIcon } from 'lucide-react'
+
 export default function Loading() {
-    return <div className="flex h-screen w-screen items-center justify-center">Загрузка ...</div>
+    return (
+        <div className="flex h-screen w-screen animate-pulse flex-col items-center justify-center gap-y-4 font-semibold">
+            <MicIcon size={48} />
+            <div>Загрузка ...</div>
+        </div>
+    )
 }
