2717a9eafb8e4c3cc76cd091e396b475e5318859
Maxim Oskolsky <oskolsky.maxim@gmail.com>
2025-07-02 11:41:34 +0300

    Fix

--- src/components/features/content/content-reviews/components/content-reviews-feed.tsx
+++ src/components/features/content/content-reviews/components/content-reviews-feed.tsx
@@ -50,7 +50,7 @@ export const ContentReviewsFeed = ({ contentId, isAuth }: ContentReviewsFeedProp
     return (
         <div className="relative flex flex-col gap-y-12">
             <div>
-                <div className="sm:absolute sm:-top-[73px] sm:right-0">
+                <div className="mb-6 sm:absolute sm:-top-[73px] sm:right-0 sm:mb-0">
                     <ContentReviewAddButton
                         contentId={contentId}
                         isAuth={isAuth}
