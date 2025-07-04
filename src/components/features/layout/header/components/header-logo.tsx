e257aa9a29d015d84e1999445df9a576891f8cf7
Maxim Oskolsky <oskolsky.maxim@gmail.com>
2025-07-04 22:15:05 +0300

    Fixes

--- src/components/features/layout/header/components/header-logo.tsx
+++ src/components/features/layout/header/components/header-logo.tsx
@@ -12,7 +12,7 @@ export const Logo = ({ className }: LogoProps) => {
         <Link href="/" className={classNames('relative flex h-full items-center gap-x-2 whitespace-nowrap', className)}>
             <MicIcon size={26} strokeWidth={2.5} />
             <div>
-                <div className="text-[10px] leading-none font-semibold text-white">Alpha</div>
+                <div className="text-[10px] leading-none font-semibold text-white">Альфа</div>
                 <div className="leading-none font-bold uppercase">ComedyPortal</div>
             </div>
         </Link>
