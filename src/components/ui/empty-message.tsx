67b18df3e32adfc44f077f3d3ee6ce60f13acf2b
Maxim Oskolsky <oskolsky.maxim@gmail.com>
2025-07-09 14:45:12 +0300

    .

--- src/components/ui/empty-message.tsx
+++ src/components/ui/empty-message.tsx
@@ -1,14 +1,7 @@
-import { BirdIcon } from 'lucide-react'
-
 type EmptyMessageProps = {
     children: React.ReactNode
 }
 
 export const EmptyMessage = ({ children }: EmptyMessageProps) => {
-    return (
-        <div className="flex items-start gap-x-4">
-            <BirdIcon strokeWidth={1} size={48} className="text-gray-400" />
-            <div className="text-gray-400">{children}</div>
-        </div>
-    )
+    return <div className="text-gray-400">{children}</div>
 }
