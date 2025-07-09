1c1fca7482e9e327d705213b87583671bb017123
Maxim Oskolsky <oskolsky.maxim@gmail.com>
2025-07-09 14:33:29 +0300

    Refactoring user page routing

--- src/components/features/landing/landing-hero.tsx
+++ src/components/features/landing/landing-hero.tsx
@@ -14,27 +14,9 @@ export const LandingHero = () => {
         <Link
             key="landing-hero-item-1"
             href="https://comedyportal.ru/content/improv_show/119"
-            className="mx-auto flex items-center justify-center sm:max-w-[1224px] sm:rounded-xl sm:px-6 lg:px-8"
+            className="mx-auto flex items-center justify-center sm:max-w-[1224px] sm:px-6 lg:px-8"
         >
-            <div
-                className="relative max-h-[400px] w-full overflow-hidden bg-gray-950"
-                style={{
-                    WebkitMaskImage: `
-                linear-gradient(to top, transparent 0%, white 20%, white 80%, transparent 100%),
-                linear-gradient(to left, transparent 0%, white 20%, white 80%, transparent 100%)
-            `,
-                    WebkitMaskComposite: 'intersect',
-                    maskComposite: 'intersect',
-                    maskImage: `
-                linear-gradient(to top, transparent 0%, white 20%, white 80%, transparent 100%),
-                linear-gradient(to left, transparent 0%, white 20%, white 80%, transparent 100%)
-            `,
-                    maskSize: 'cover',
-                    WebkitMaskSize: 'cover',
-                    maskRepeat: 'no-repeat',
-                    WebkitMaskRepeat: 'no-repeat',
-                }}
-            >
+            <div className="relative max-h-[400px] w-full bg-gray-950 sm:overflow-hidden sm:rounded-2xl">
                 <Image
                     src="/images/promo/promo.jpg"
                     alt="Три Гиены"
