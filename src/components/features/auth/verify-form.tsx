22f5196def7dd4b342fd3bac767869a0796c496f
Maxim Oskolsky <oskolsky.maxim@gmail.com>
2025-07-14 10:53:46 +0300

    New verify form

--- src/components/features/auth/verify-form.tsx
+++ src/components/features/auth/verify-form.tsx
@@ -1,14 +1,16 @@
 'use client'
 
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 
+import classNames from 'classnames'
 import { ArrowLeftIcon } from 'lucide-react'
 import * as yup from 'yup'
 
 import { VerifyTimer } from '@/components/features/auth/verify-timer'
 import { Button } from '@/components/ui/forms/button'
-import { Input } from '@/components/ui/forms/input'
 import { messages } from '@/messages'
+
+const CODE_LENGTH = 6
 
 const validationSchema = yup.object().shape({
     otp: yup
@@ -27,16 +29,58 @@ type SignUpFormProps = {
 }
 
 export const VerifyForm = ({ email, status, isLoading, onVerifyOtp, onResendOtp, onBack }: SignUpFormProps) => {
-    const [otp, setOtp] = useState('')
+    const [otpValues, setOtpValues] = useState<string[]>(Array(CODE_LENGTH).fill(''))
     const [errors, setErrors] = useState<{ otp?: string }>({})
+    const inputsRef = useRef<(HTMLInputElement | null)[]>([])
+
+    const focusInput = (index: number) => {
+        if (inputsRef.current[index]) {
+            inputsRef.current[index]?.focus()
+        }
+    }
+
+    const handleChange = (index: number, value: string) => {
+        if (!/^\d?$/.test(value)) return
+
+        const updatedValues = [...otpValues]
+        updatedValues[index] = value
+        setOtpValues(updatedValues)
+
+        if (value && index < CODE_LENGTH - 1) {
+            focusInput(index + 1)
+        }
+    }
+
+    const handlePaste = (e: React.ClipboardEvent<HTMLInputElement>) => {
+        const pastedData = e.clipboardData.getData('Text').replace(/\D/g, '').slice(0, CODE_LENGTH)
+        if (pastedData.length === 0) return
+
+        const newOtp = [...otpValues]
+        for (let i = 0; i < CODE_LENGTH; i++) {
+            newOtp[i] = pastedData[i] || ''
+        }
+        setOtpValues(newOtp)
+
+        const nextEmpty = newOtp.findIndex(char => !char)
+        if (nextEmpty !== -1) {
+            focusInput(nextEmpty)
+        } else {
+            focusInput(CODE_LENGTH - 1)
+        }
+    }
+
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>, index: number) => {
+        if (e.key === 'Backspace' && !otpValues[index] && index > 0) {
+            focusInput(index - 1)
+        }
+    }
 
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault()
 
-        const formData = { otp }
-
+        const otp = otpValues.join('')
         try {
-            await validationSchema.validate(formData, { abortEarly: false })
+            await validationSchema.validate({ otp }, { abortEarly: false })
             setErrors({})
 
             const form = new FormData()
@@ -67,8 +111,7 @@ export const VerifyForm = ({ email, status, isLoading, onVerifyOtp, onResendOtp,
                 <div className="mb-4 rounded-lg border border-red-100 bg-red-50 p-4 text-center text-sm text-red-500">
                     {status === 'EXPIRED_USER_INPUT_CODE_ERROR' && 'Код подтверждения устарел'}
                     {status === 'INCORRECT_USER_INPUT_CODE_ERROR' && 'Неверный код подтверждения'}
-                    {status === 'RESTART_FLOW_ERROR' && messages.COMMON_ERROR}
-                    {status === 'SIGN_IN_UP_NOT_ALLOWED' && messages.COMMON_ERROR}
+                    {(status === 'RESTART_FLOW_ERROR' || status === 'SIGN_IN_UP_NOT_ALLOWED') && messages.COMMON_ERROR}
                 </div>
             )}
 
@@ -77,15 +120,34 @@ export const VerifyForm = ({ email, status, isLoading, onVerifyOtp, onResendOtp,
                     <label className="block text-sm font-semibold text-gray-700">Код</label>
                     <VerifyTimer onResendOtp={onResendOtp} />
                 </div>
-                <Input
-                    type="text"
-                    name="otp"
-                    autoFocus
-                    value={otp}
-                    error={errors.otp}
-                    disabled={isLoading}
-                    onChange={e => setOtp(e.target.value)}
-                />
+
+                <div className="flex items-center justify-between gap-x-2">
+                    {Array.from({ length: CODE_LENGTH }).map((_, i) => (
+                        <input
+                            key={i}
+                            ref={el => {
+                                inputsRef.current[i] = el
+                            }}
+                            type="text"
+                            inputMode="numeric"
+                            maxLength={1}
+                            value={otpValues[i]}
+                            disabled={isLoading}
+                            className={classNames(
+                                'size-12 rounded-lg border text-center text-lg disabled:cursor-no-drop disabled:opacity-30',
+                                {
+                                    'border-red-500': errors.otp,
+                                    'border-gray-300 hover:border-gray-400 focus:border-gray-400': !errors.otp,
+                                },
+                                'focus:ring-2 focus:ring-blue-500 focus:outline-none',
+                            )}
+                            onChange={e => handleChange(i, e.target.value)}
+                            onPaste={handlePaste}
+                            onKeyDown={e => handleKeyDown(e, i)}
+                        />
+                    ))}
+                </div>
+                {errors.otp && <p className="mt-1 text-xs text-red-500">{errors.otp}</p>}
             </div>
 
             <Button type="submit" className="mb-6 w-full" disabled={isLoading}>
