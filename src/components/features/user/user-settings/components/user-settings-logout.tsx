cb63d498c7192ccfaa700d0fc986fc01af829457
Maxim Oskolsky <oskolsky.maxim@gmail.com>
2025-07-15 18:53:39 +0300

    Login and logout reset api

--- src/components/features/user/user-settings/components/user-settings-logout.tsx
+++ src/components/features/user/user-settings/components/user-settings-logout.tsx
@@ -1,18 +1,23 @@
 'use client'
 
+import { useDispatch } from 'react-redux'
+
 import { LogOutIcon } from 'lucide-react'
 import Session from 'supertokens-web-js/recipe/session'
 
 import { useRouter } from 'next/navigation'
 
 import { messages } from '@/messages'
+import { api } from '@/redux/services/api'
 
 export const UserSettingsLogout = () => {
     const router = useRouter()
+    const dispatch = useDispatch()
 
     const signOut = async () => {
         try {
             await Session.signOut()
+            dispatch(api.util.resetApiState())
             router.push('/')
             router.refresh()
         } catch {
