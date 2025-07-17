0607e6e218cfdd0779178ba0926a319791a4b002
Maxim Oskolsky <oskolsky.maxim@gmail.com>
2025-07-17 20:09:18 +0300

    hide instagram link

--- src/components/features/layout/footer/footer.tsx
+++ src/components/features/layout/footer/footer.tsx
@@ -8,7 +8,7 @@ export const Footer = async () => {
             </div>
             <footer className="border-t border-gray-300">
                 <div className="wrapper-lg flex flex-col items-center justify-between gap-2 py-6 text-sm sm:flex-row sm:gap-4">
-                    <div>&copy; 2025 Comedy Portal</div>
+                    <div className="font-bold">&copy; 2025 Comedy Portal</div>
                     <nav className="flex flex-col gap-x-4 sm:flex-row">
                         <Link href="/legal/terms-of-use" className="text-gray-500 hover:text-gray-950">
                             Пользовательское соглашение
--- src/components/features/layout/header/components/header-submit-content.tsx
+++ src/components/features/layout/header/components/header-submit-content.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { MessageCircleIcon, PlusCircleIcon } from 'lucide-react'
+import { PlusCircleIcon } from 'lucide-react'
 
 import { useRouter } from 'next/navigation'
 
