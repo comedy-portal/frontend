959ac047c2a8484d5283e6c75208acd177bc5d87
Maxim Oskolsky <oskolsky.maxim@gmail.com>
2025-07-22 12:19:33 +0300

    Fix skeleton

--- src/components/features/top-content/components/top-content-feed-skeleton.tsx
+++ src/components/features/top-content/components/top-content-feed-skeleton.tsx
@@ -1,12 +1,21 @@
+import classNames from 'classnames'
+
 import { ContentBlockSkeleton } from '@/components/ui/content-block/content-block-skeleton'
 
 const BLOCK_QTY = 8
 
 export const TopContentFeedSkeleton = () => {
     return (
-        <div role="status" className="grid animate-pulse grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 lg:grid-cols-3">
+        <div
+            role="status"
+            className={classNames(
+                'animate-pulse',
+                'lg:block lg:space-y-2',
+                'grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6',
+            )}
+        >
             {Array.from({ length: BLOCK_QTY }).map((_, i) => (
-                <ContentBlockSkeleton key={`top-feed-item-skeleton-${i}`} />
+                <ContentBlockSkeleton key={`top-content-feed-item-skeleton-${i}`} />
             ))}
         </div>
     )
