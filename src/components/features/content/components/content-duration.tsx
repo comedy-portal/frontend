64801871ab52d085f8b0fb2887194a2a32480729
Maxim Oskolsky <oskolsky.maxim@gmail.com>
2025-07-25 15:19:44 +0300

    Duration format

--- src/components/features/content/components/content-duration.tsx
+++ src/components/features/content/components/content-duration.tsx
@@ -2,6 +2,17 @@ type ContentDurationProps = {
     duration: number | null
 }
 
+const formatDuration = (duration: number): string => {
+    const hours = Math.floor(duration / 60)
+    const minutes = duration % 60
+
+    if (hours > 0) {
+        return `${hours} ч${minutes > 0 ? ` ${minutes} мин.` : ''}`
+    }
+
+    return `${minutes} мин.`
+}
+
 export const ContentDuration = ({ duration }: ContentDurationProps) => {
     if (duration === null) {
         return null
@@ -10,7 +21,7 @@ export const ContentDuration = ({ duration }: ContentDurationProps) => {
     return (
         <section className="space-y-2">
             <h3 className="font-bold">Продолжительность</h3>
-            <p>{duration} мин</p>
+            <p>{formatDuration(duration)}</p>
         </section>
     )
 }
