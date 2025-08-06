f83019fd0d3fc054563d53a5884c9587ab649c70
Maxim Oskolsky <oskolsky.maxim@gmail.com>
2025-08-06 10:11:02 +0300

    Comedian image fallback

--- src/components/ui/comedian-block/comedian-block.tsx
+++ src/components/ui/comedian-block/comedian-block.tsx
@@ -1,5 +1,6 @@
-import Image from 'next/image'
 import Link from 'next/link'
+
+import { ImageWithFallback } from '../image-with-fallback'
 
 type ComedianBlockType = {
     slug: string
@@ -11,12 +12,12 @@ type ComedianBlockType = {
 export const ComedianBlock = ({ slug, name, surname, isAgent }: ComedianBlockType) => {
     return (
         <Link href={`/comedians/${slug}`} className="flex flex-col gap-y-2" target="_blank">
-            <Image
+            <ImageWithFallback
                 src={`/images/comedians/${slug}.jpg`}
-                alt={`${name} ${surname}`}
                 width={100}
                 height={100}
                 className="aspect-square w-full rounded-lg"
+                alt={`${name} ${surname}`}
             />
 
             <div className="font-semibold">
