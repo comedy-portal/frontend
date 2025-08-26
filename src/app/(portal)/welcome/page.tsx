2f62c9c18d2988aa0b8a097f393b41ac7bd80a5b
Maxim Oskolsky <oskolsky.maxim@gmail.com>
2025-08-26 17:53:51 +0300

    .

--- src/app/(portal)/welcome/page.tsx
+++ src/app/(portal)/welcome/page.tsx
@@ -1,5 +1,13 @@
 import { Welcome } from '@/components/features/welcome/welcome'
+import { getUserData } from '@/services/user/user'
+import { withAuth } from '@/utils/supertokens/with-auth'
 
 export default function WelcomePage() {
-    return <Welcome />
+    return withAuth({
+        getAuthData: async () => {
+            const activeUser = await getUserData()
+            return { userName: activeUser.username }
+        },
+        render: ({ isAuth, data }) => <Welcome username={data?.userName} isAuth={isAuth} />,
+    })
 }
