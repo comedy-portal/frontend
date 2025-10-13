bbbb8199bda6071ae6d37f2012b8f1bb1424a1d8
Maxim Oskolsky <oskolsky.maxim@gmail.com>
2025-10-13 15:12:43 +0300

    tripadvancer banner

--- src/components/features/layout/alpha-banner/alpha-banner.tsx
+++ src/components/features/layout/alpha-banner/alpha-banner.tsx
@@ -20,7 +20,7 @@ export const AlphaBanner = () => {
     }
 
     return (
-        <div className="relative bg-blue-50 px-4 py-2 text-center text-sm sm:px-6 lg:px-8">
+        <div className="relative bg-blue-50 px-8 py-2 text-center text-sm">
             <Link
                 href="https://tripadvancer.com"
                 className="text-gray-700 hover:text-gray-950"
@@ -31,7 +31,7 @@ export const AlphaBanner = () => {
                 путешествий!
             </Link>
             <div
-                className="hover:text-black-950 absolute top-1/2 right-4 h-4 w-4 -translate-y-1/2 cursor-pointer text-gray-500 hover:text-gray-700"
+                className="hover:text-black-950 absolute top-2 right-2 h-4 w-4 cursor-pointer text-gray-500 hover:text-gray-700 lg:top-1/2 lg:right-4 lg:-translate-y-1/2"
                 onClick={handleClose}
             >
                 <XIcon size={16} />
