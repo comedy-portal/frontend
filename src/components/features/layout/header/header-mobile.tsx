d0a2022e99607b3ceb28d0106af43f4f71a0f434
Maxim Oskolsky <oskolsky.maxim@gmail.com>
2025-11-18 11:40:49 +0200

    .

--- src/components/features/layout/header/header-mobile.tsx
+++ src/components/features/layout/header/header-mobile.tsx
@@ -37,7 +37,7 @@ export const HeaderMobile = ({ username, isAuth }: HeaderMobileProps) => {
             </Link>
 
             <div className="flex items-center gap-x-4">
-                <NotificationsBell />
+                {isAuth && username && <NotificationsBell />}
 
                 {isOpen ? (
                     <XIcon className="text-gray-300" onClick={closeMobileMenu} />
