7cd23c38ba380f7fa25fcdfc69ace21b2136c169
Maxim Oskolsky <oskolsky.maxim@gmail.com>
2026-01-08 21:34:11 +0200

    Cookie Consent

--- src/app/(portal)/layout.tsx
+++ src/app/(portal)/layout.tsx
@@ -1,4 +1,5 @@
 import { AlphaBanner } from '@/components/features/layout/alpha-banner/alpha-banner'
+import { CookieConsent } from '@/components/features/layout/cookie-canner/cookie-consent'
 import { FooterAuthWrapper } from '@/components/features/layout/footer/footer-auth-wrapper'
 import { HeaderAuthWrapper } from '@/components/features/layout/header/header-auth-wrapper'
 import ScrollToTopButton from '@/components/features/layout/scroll-to-top-button/scroll-to-top-button'
@@ -11,6 +12,7 @@ export default function PortalLayout({ children }: Readonly<{ children: React.Re
             <main className="flex-1">{children}</main>
             <FooterAuthWrapper />
             <ScrollToTopButton />
+            <CookieConsent />
         </div>
     )
 }
