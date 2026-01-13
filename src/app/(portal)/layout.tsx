bc16caac3e5848437dd14563bea479506662ea32
Maxim Oskolsky <oskolsky.maxim@gmail.com>
2026-01-13 15:20:29 +0200

    TG Banner

--- src/app/(portal)/layout.tsx
+++ src/app/(portal)/layout.tsx
@@ -3,6 +3,7 @@ import { CookieConsent } from '@/components/features/layout/cookie-canner/cookie
 import { FooterAuthWrapper } from '@/components/features/layout/footer/footer-auth-wrapper'
 import { HeaderAuthWrapper } from '@/components/features/layout/header/header-auth-wrapper'
 import ScrollToTopButton from '@/components/features/layout/scroll-to-top-button/scroll-to-top-button'
+import { TGBanner } from '@/components/features/layout/tg-banner/tg-banner'
 
 export default function PortalLayout({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
@@ -10,6 +11,7 @@ export default function PortalLayout({ children }: Readonly<{ children: React.Re
             <AlphaBanner />
             <HeaderAuthWrapper />
             <main className="flex-1">{children}</main>
+            <TGBanner />
             <FooterAuthWrapper />
             <ScrollToTopButton />
             <CookieConsent />
---
+++ src/components/features/layout/tg-banner/tg-banner.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+
+export const TGBanner = () => {
+    const router = useRouter()
+
+    return (
+        <div className="bg-gray-700 py-8 sm:py-16">
+            <div className="wrapper">
+                <div className="m-auto space-y-8 text-center sm:w-2/3">
+                    <p className="text-lg font-bold text-white">
+                        Будьте в&nbsp;курсе всех изменений на&nbsp;Камеди Портал! Подписывайтесь на&nbsp;наш
+                        Telegram-канал и&nbsp;получайте новости о&nbsp;новых функциях и&nbsp;обновлениях.
+                    </p>
+                    <Link
+                        href="https://t.me/comedyportal"
+                        className="inline-flex h-12 w-full cursor-pointer items-center justify-center rounded-lg border border-white px-6 font-bold text-nowrap text-white sm:w-auto"
+                        target="_blank"
+                    >
+                        Подписаться
+                    </Link>
+                </div>
+            </div>
+        </div>
+    )
+}
