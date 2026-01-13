16d23f10ae7f09a9cda45f6ad695ba3b64d3aefe
Maxim Oskolsky <oskolsky.maxim@gmail.com>
2026-01-13 12:27:03 +0200

    How to add content

--- src/content/blog/content-submit.tsx
+++ src/content/blog/content-submit.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import Link from 'next/link'
 
 import { BlogArticle } from '@/utils/types/blog'
@@ -53,23 +54,63 @@ const article: BlogArticle = {
     title: 'Как предложить контент для Камеди Портал',
     content: (
         <>
-            <p>Мы&nbsp;ценим вклад нашей аудитории и&nbsp;комиков. Если у&nbsp;тебя есть идея для нового контента&nbsp;&mdash; будь&nbsp;то спешл, юмористический подкаст или шоу, ты&nbsp;можешь легко предложить его для добавления в&nbsp;сервис.</p>
-            <div>
-                <strong>Важно</strong>
-                <br />
-                <p>Мы&nbsp;добавляем только контент, который соответствует нашим типам и&nbsp;описанию форматов. Подробности о&nbsp;подходящем контенте можно посмотреть <Link href="/about/formats" className='text-blue-500 hover:text-blue-700' target='_blank'>здесь</Link>.</p>
+            <p>Мы&nbsp;ценим вклад нашей аудитории и&nbsp;комиков. Если у&nbsp;Вас есть идея для нового контента&nbsp;&mdash; будь&nbsp;то спешл, юмористический подкаст или шоу, Вы&nbsp;можете легко предложить его для добавления в&nbsp;сервис.</p>
+            <div className="space-y-4">
+                <h2 className="text-lg font-semibold">Важно</h2>
+                <p>Мы&nbsp;добавляем только контент, который соответствует нашим типам и&nbsp;описанию форматов. Подробности о&nbsp;подходящем контенте можно посмотреть <Link href="/about/formats" className="text-blue-500 hover:text-blue-700" target="_blank">здесь</Link>.</p>
             </div>
-            <div>
-                <strong>Как это работает?</strong>
-                <br />
+
+            <div className="space-y-4">
+                <h2 className="text-lg font-semibold">Как это работает</h2>
                 <ul className="list-disc pl-12">
-                    <li>Найди кнопку «Предложить контент»</li>
+                    <li>Найдите кнопку &laquo;Предложить контент&raquo;</li>
                     <li>
-                        нажимаешь кнопку <strong>«Подписаться»</strong>
+                        На&nbsp;большом экране: это кнопка &laquo;+&raquo;, она всегда видна в&nbsp;верхней части страницы.
                     </li>
-                    <li>готово</li>
+                    <li>
+                        На&nbsp;мобильных устройствах: нажмите на&nbsp;меню в&nbsp;правом верхнем углу, там найдёте кнопку &laquo;Предложить контент&raquo;. Просто нажмите&nbsp;её, и&nbsp;откроется удобная форма для предложений.
+                    </li>
                 </ul>
             </div>
+            
+            <div className="space-y-4">
+                <h2 className="text-lg font-semibold">Заполните форму</h2>
+                <p>В&nbsp;форме есть два основных поля:</p>
+                <ul className="list-disc pl-12">
+                    <li><strong>Ссылка на&nbsp;видео</strong>&nbsp;&mdash; укажите прямую ссылку на&nbsp;контент, который хотите предложить.</li>
+                    <li><strong>Описание видео</strong>&nbsp;&mdash; кратко расскажите, о&nbsp;чём видео, чтобы мы&nbsp;понимали, что это за&nbsp;материал и&nbsp;зачем его добавлять.</li>
+                </ul>
+            </div>
+            
+            <div className="space-y-4">
+                <h2 className="text-lg font-semibold">Для комиков и авторов</h2>
+                <p>Если Вы&nbsp;автор контента или хотите дать больше информации, добавьте информацию об&nbsp;авторе контента:</p>
+                <ul className="list-disc pl-12">
+                    <li>Ссылка на&nbsp;социальные сети или личный сайт</li>
+                    <li>Краткая биография (откуда комик, когда родился, интересные факты)</li>
+                    <li>Любые достижения или участие в&nbsp;проектах/шоу</li>
+                </ul>
+                <p>Это помогает нам добавлять больше полезной информации о&nbsp;комике и&nbsp;делать каталог качественным и&nbsp;интересным для всех пользователей.</p>
+            </div>
+
+            
+            <div className="space-y-4">
+                <h2 className="text-lg font-semibold">Отправьте предложение</h2>
+                <p>После заполнения формы просто нажмите &laquo;Отправить&raquo;. Мы&nbsp;проверим контент&nbsp;и, если он&nbsp;подходит, добавим его на&nbsp;Камеди Портал.</p>
+            </div>
+
+            <div className="w-full overflow-hidden rounded shadow-2xl">
+                <Image
+                    src="/images/welcome/content-submit.jpg"
+                    width={2560}
+                    height={2560}
+                    alt="Как предложить контент для Камеди Портал"
+                />
+            </div>
+
+            <p className="italic">
+                <strong>Совет:</strong> чем точнее и&nbsp;информативнее описание, тем быстрее мы&nbsp;сможем добавить материал в&nbsp;каталог. Даже короткий комментарий к&nbsp;видео помогает нам понять контент и&nbsp;его контекст.
+            </p>
         </>
     ),
 }
