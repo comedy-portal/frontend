0ea4c1a1ce1ef2393181284b86a63be5a4632dde
Maxim Oskolsky <oskolsky.maxim@gmail.com>
2026-05-04 19:53:31 +0300

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,16 @@
 import classNames from 'classnames'
 import type { Metadata, Viewport } from 'next'
 
+import { GoogleAnalytics } from '@next/third-parties/google'
 import { Roboto } from 'next/font/google'
 
+import YandexMetrika from '@/components/features/common/yandex-metrika'
+import { TailwindIndicator } from '@/components/ui/tailwind-indicator'
 import { createMetadata } from '@/utils/helpers/metadata'
 
 import './globals.css'
+
+import { Providers } from './providers'
 
 const roboto = Roboto({
     subsets: ['latin', 'cyrillic'],
@@ -40,7 +45,7 @@ export const metadata: Metadata = {
     },
 }
 
-export default function RootLayout() {
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
         <html lang="ru" className={classNames('h-full', roboto.className)}>
             <head>
@@ -52,7 +57,10 @@ export default function RootLayout() {
                 <link rel="apple-touch-icon" href="/icons/icon-192.png" />
             </head>
             <body className="h-full antialiased">
-                <div>Test test test</div>
+                <Providers>{children}</Providers>
+                <TailwindIndicator />
+                <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID as string} />
+                <YandexMetrika />
             </body>
         </html>
     )
